580d6d5c6b1fb08892b4b81e93a701d3b0846fd8
Anthony Phan <89223273+anthphan@users.noreply.github.com>
2024-04-06 18:29:52 -0400

    add like button function

--- posts/static/posts/main.js
+++ posts/static/posts/main.js
@@ -1,53 +1,118 @@
 console.log("hello world")
 
-const helloWorldBox = document.getElementById("hello-world")
 const postsBox = document.getElementById("posts-box")
 const spinnerBox = document.getElementById("spinner-box")
+const loadBtn = document.getElementById("load-btn")
+const endBox = document.getElementById("end-box")
 
 $.ajax({
     type: 'GET',
     url: '/hello-world/',
     success: function(response){
         console.log('success', response.text)
-        helloWorldBox.textContent = response.text
     },
     error: function(error) {
         console.log('error', error)
     }
 })
 
-$.ajax({
-    type: 'GET',
-    url: '/data/',
-    success: function(response) {
-        console.log(response)
-        const data = response.data
-        setTimeout(() => {
-            spinnerBox.classList.add("not-visible")
-            console.log(data)
-            data.forEach(el => {
-                postsBox.innerHTML += `
-                    <div class="card mb-2">
-                        <div class="card-body">
-                            <h5 class="card-title">${el.title}</h5>
-                            <p class="card-text">${el.body}</p>
-                        </div>
-                        <div class="card-footer">
-                            <div class="row">
-                                <div class="col-1">
-                                    <a href="#" class="btn btn-primary">Details</a>
-                                </div>
-                                <div class="col-1">
-                                    <a href="#" class="btn btn-primary">Like</a>
+const getCookie = (name) =>{
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+const csrftoken = getCookie('csrftoken');
+
+const likeUnlikePosts = () => {
+    const likeUnlikeForms = [...document.getElementsByClassName('like-unlike-forms')]
+    likeUnlikeForms.forEach(form=> form.addEventListener('submit', e=> {
+        e.preventDefault();
+        const clickedId = e.target.getAttribute('data-form-id')
+        const clickedBtn = document.getElementById(`like-unlike-${clickedId}`)
+
+        $.ajax({
+            type: "POST",
+            url: "/like-unlike/",
+            data: {
+                'csrfmiddlewaretoken': csrftoken,
+                'pk': clickedId,
+            },
+            success: function(response) {
+                console.log(response)
+            },
+            error: function(error) {
+                console.log(error)
+            }
+        })
+
+    }))
+}
+
+let visible = 3
+
+const getData = () => {
+    $.ajax({
+        type: 'GET',
+        url: `/data/${visible}`,
+        success: function(response) {
+            console.log(response)
+            const data = response.data
+            setTimeout(() => {
+                spinnerBox.classList.add("not-visible")
+                console.log(data)
+                data.forEach(el => {
+                    postsBox.innerHTML += `
+                        <div class="card mb-2">
+                            <div class="card-body">
+                                <h5 class="card-title">${el.title}</h5>
+                                <p class="card-text">${el.body}</p>
+                            </div>
+                            <div class="card-footer">
+                                <div class="row">
+                                    <div class="col-2">
+                                        <a href="#" class="btn btn-primary">Details</a>
+                                    </div>
+                                    <div class="col-2">
+                                        <form class="like-unlike-forms" data-form-id="${el.id}">
+                                            <button href="#" class="btn btn-primary id="like-unlike-${el.id}">${el.liked ? `Unlike (${el.count})`: `Like (${el.count})`}</button>
+                                        </form>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                `
-            });
-        }, 100)
-    },
-    error: function(error) {
-        console.log(error)
-    }
+                    `
+                });
+                likeUnlikePosts();
+            }, 100)
+            console.log(response.size)
+            if (response.size === 0) {
+                endBox.textContent = "no posts added yet ... "
+            } else if (response.size <= visible) {
+                loadBtn.classList.add('not-visibile')
+                endBox.textContent = "no more posts to load"
+            }
+        },
+        error: function(error) {
+            console.log(error)
+        }
+    })
+}
+
+
+loadBtn.addEventListener('click', () => {
+    spinnerBox.classList.remove('not-visible')
+    visible += 3
+    getData()
 })
+
+getData()

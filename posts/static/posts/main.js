e8ad069e07b62f51f54bdca71b77804147ae618c
Anthony Phan <89223273+anthphan@users.noreply.github.com>
2024-04-06 16:04:46 -0400

    add spinner for AJAX calls

--- posts/static/posts/main.js
+++ posts/static/posts/main.js
@@ -1,5 +1,38 @@
 console.log("hello world")
 
 const helloWorldBox = document.getElementById("hello-world")
+const postsBox = document.getElementById("posts-box")
+const spinnerBox = document.getElementById("spinner-box")
 
-helloWorldBox.innerHTML = "hello <b>world</b>"
+$.ajax({
+    type: 'GET',
+    url: '/hello-world/',
+    success: function(response){
+        console.log('success', response.text)
+        helloWorldBox.textContent = response.text
+    },
+    error: function(error) {
+        console.log('error', error)
+    }
+})
+
+$.ajax({
+    type: 'GET',
+    url: '/data/',
+    success: function(response) {
+        console.log(response)
+        const data = response.data
+        setTimeout(() => {
+            spinnerBox.classList.add("not-visible")
+            console.log(data)
+            data.forEach(el => {
+                postsBox.innerHTML += `
+                    ${el.title} - <b>${el.body}</b><br>
+                `
+            });
+        }, 100)
+    },
+    error: function(error) {
+        console.log(error)
+    }
+})

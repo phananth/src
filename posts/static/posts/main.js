dc95ad71cbe92cc92ebc192acddcf8a4f0866283
Anthony Phan <89223273+anthphan@users.noreply.github.com>
2024-04-06 21:28:42 -0400

    add update and delete post functionality

--- posts/static/posts/main.js
+++ posts/static/posts/main.js
@@ -10,6 +10,8 @@ const title = document.getElementById("id_title")
 const body = document.getElementById("id_body")
 const csrf = document.getElementsByName("csrfmiddlewaretoken")
 
+const url = window.location.href
+
 const alertBox = document.getElementById("alert-box")
 console.log('csrf', csrf[0].value)
 
@@ -89,7 +91,7 @@ const getData = () => {
                             <div class="card-footer">
                                 <div class="row">
                                     <div class="col-2">
-                                        <a href="#" class="btn btn-primary">Details</a>
+                                        <a href="${url}${el.id}" class="btn btn-primary">Details</a>
                                     </div>
                                     <div class="col-2">
                                         <form class="like-unlike-forms" data-form-id="${el.id}">
@@ -160,6 +162,7 @@ postForm.addEventListener('submit', e=> {
             likeUnlikePosts()
             $('#addPostModal').modal('hide')
             handleAlert('success', 'New post added!')
+            postForm.reset()
 
         },
         error: function(error){
--- posts/urls.py
+++ posts/urls.py
@@ -3,8 +3,11 @@ from .views import (
     post_list_and_create,
     load_post_data_view,
     like_unlike_post,
+    post_detail,
+    post_detail_data_view,
+    update_post,
+    delete_post,
     
-    hello_world_view
 )
 
 app_name = 'posts'
@@ -12,7 +15,11 @@ app_name = 'posts'
 urlpatterns = [
     path('', post_list_and_create, name='main-board'),
     path('like-unlike/', like_unlike_post, name='like-unlike'),
-    path('data/<int:num_posts>/', load_post_data_view, name='posts-data'),
+    path('<pk>/', post_detail, name='post-detail'),
+    path('<pk>/update/', update_post, name='post-update'),
+    path('<pk>/delete/', delete_post, name='post-delete'),
 
-    path('hello-world/', hello_world_view, name='hello world')
+    path('data/<int:num_posts>/', load_post_data_view, name='posts-data'),
+    path('<pk>/data/', post_detail_data_view, name='post-detail-data'),
+
 ]
